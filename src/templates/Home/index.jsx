479e6a809d7120e5cde4c50c7761fe6188d3ca64
Mateus Couto <mateuscouto98@outlook.com>
2021-10-30 20:56:55 -0300

    Load More Posts

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -4,11 +4,14 @@ import "./styles.css";
 
 import { Posts } from "../../componnets/Posts";
 import { loadPosts } from "../../utils/loadPosts";
+import { Button } from "../../componnets/Button";
 
 export class Home extends Component {
   state = {
     posts: [],
     allPosts: [],
+    page: 0,
+    postsPerPage: 3,
   };
 
   async componentDidMount() {
@@ -16,11 +19,22 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
+
     const postsAndPhotos = await loadPosts();
     this.setState({
-      posts: postsAndPhotos.slice(0, 4),
+      posts: postsAndPhotos.slice(page, postsPerPage),
       allPosts: postsAndPhotos,
     });
+  };
+
+  loadMorePosts = () => {
+    const { page, postsPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
   };
 
   render() {
@@ -29,6 +43,7 @@ export class Home extends Component {
     return (
       <section className="container">
         <Posts posts={posts} />
+        <Button text="Load more posts" onClick={this.loadMorePosts} />
       </section>
     );
   }
